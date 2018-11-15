93927fce9ff9e4a8207ea2ab39a7ef414bb1cac9
Mr.L@mbert_13 <lam_13@mail.ru>
2018-11-15 09:22:23 +0300

    lesson 2 ex1 modify

--- lesson2/Exercise1/WeightProd.php
+++ lesson2/Exercise1/WeightProd.php
@@ -4,9 +4,4 @@ require_once 'config.php';
 
 class WeightProd extends Product {
 
-  function revenue(float $count) {
-    $message = 'Выручка составила: ';
-    return $message . ($this->totalPrice() * $count);
-  }
-
 }
