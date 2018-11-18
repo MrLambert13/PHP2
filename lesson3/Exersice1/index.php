6e45085022fe86e638d73bda05dc99a94ba90217
Mr.L@mbert_13 <Lam_13@mail.ru>
2018-11-18 13:35:56 +0300

    lesson3 ex1 complete

--- lesson3/Exersice1/index.php
+++ lesson3/Exersice1/index.php
@@ -4,9 +4,41 @@ require 'vendor/autoload.php';
 
 use App\View\Page;
 
-$page = new Page('home');
+include 'vendor/twig/autoload.php';
 
-echo $page->render([
+// подключение к бд
+try {
+  $dbh = new PDO('mysql:dbname=gallery;host=mrlambert.ru', 'root', '123456');
+} catch (PDOException $e) {
+  echo "Error: Could not connect. " . $e->getMessage();
+}
+
+// установка error режима
+$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+// выполняем запрос
+try {
+  // формируем SELECT запрос
+  // в результате каждая строка таблицы будет объектом
+  $sql = "SELECT * FROM image";
+  $sth = $dbh->query($sql);
+  //в массив
+  while ($row = $sth->fetchAll()) {
+    $data[] = $row;
+  }
+  // закрываем соединение
+  unset($dbh);
+
+} catch (Exception $e) {
+  die ('ERROR: ' . $e->getMessage());
+}
+
+//путь и названия для работы с локальной галереей
+$pathToImgFolder = 'img/gallery/';
+$arrImg = array_diff(scandir($pathToImgFolder), array('..', '.'));
+
+//$page = new Page('home');
+/*echo $page->render([
     'currentDate' => date('d-m-Y'),
     'error' => false,
     'users' => [
@@ -23,4 +55,12 @@ echo $page->render([
             'email' => 'admin3@com.com'
         ],
     ],
+]);*/
+
+$gallery = new Page('gallery');
+
+echo $gallery->render([
+  'arrImg' => $arrImg,
+  'path' => $pathToImgFolder,
+  'data' => $data[0]
 ]);
