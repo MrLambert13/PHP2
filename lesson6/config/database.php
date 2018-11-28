b33570cc6d1f66df21c3ae90048f7273a87c758f
Mr.L@mbert_13 <Lam_13@mail.ru>
2018-11-28 19:17:11 +0300

    Lesson 5 Ex3 complete

---
+++ lesson6/config/database.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by Artem Manchenkov
+ * artyom@manchenkoff.me
+ * manchenkoff.me © 2018
+ */
+
+/**
+ * Настройки подключения к базе данных
+ */
+return [
+    'database' => [
+        'driver' => 'pdo_mysql',
+        'host' => 'mrlambert.ru',
+        'user' => 'root',
+        'password' => '123456',
+        'dbname' => 'geek_project',
+    ],
+];
